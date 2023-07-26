ef782cfeacc6e149462a5e09708dd08c34536435
s1Develop <sungwon.dev@gmail.com>
2023-07-25 21:07:36 -0700

    07/22/23

---
+++ Easy/RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,55 @@
+"""
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+"""
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+
+        #index 1부터 마지막 index까지 즉 index 0은 제외
+        for i in range(1, len(nums)):
+
+            #현재 index의 element가 그전 index의 element값이랑 다를경우
+            if nums[i] != nums[i-1]:
+                #num[j]값은 현재 index의 element값으로 바뀌고
+                nums[j] = nums[i]
+                
+                #j는 +1추가가 된다.
+                j += 1
+        #대충 nums의 index를 j로 계속 replace시키는 것
+        return j
