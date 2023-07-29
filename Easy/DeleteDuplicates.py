edaaec2be29d7e3f37f31695bdbc13ee1fece471
s1Develop <sungwon.dev@gmail.com>
2023-07-29 00:00:13 -0700

    07/29/2023

---
+++ Easy/DeleteDuplicates.py
@@ -0,0 +1,39 @@
+"""
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,1,2]
+Output: [1,2]
+Example 2:
+
+
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return head
+
+
+        current = head.next
+        prev = head
+
+        while current != None:
+            if current.val = prev.val:
+                prev.next = current.next
+                current = current.next
+            else:
+                current = current.next
+                prev = prev.next
+        return head
