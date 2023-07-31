a1da69144d82ef51e50ac8995a788286e13e6ab3
s1Develop <sungwon.dev@gmail.com>
2023-07-30 23:44:15 -0700

    07/30/2023

--- Easy/ClimbStairs.py
+++ Easy/ClimbStairs.py
@@ -22,10 +22,21 @@ Explanation: There are three ways to climb to the top.
 """
 
 #결국 어렵게 생각할 필요가 없다.
-#그냥 recursion을 써서 돌리면 끝...
+#그냥 recursion을 써서 돌리면 끝
+#하지만 recursion을 써버리면 time complexity가 2^n이 되기에
+# 320에서 배운 memoization 즉 recursion중에서 redundant한 calculation들을 없애주는 것.
+
 class Solution:
     def climbStairs(self, n: int) -> int:
+        #memo is an unordered mapto store the already computed results for each step n.
+        memo = {}
+        return self.helper(n, memo)
+    
+
+
+    def helper(self, n: int, memo: dict[int, int]) -> int:
         if n == 0 or n == 1:
             return 1
-        else:
-            return self.climbStairs(n-1) + self.climbStairs(n-2)
+        if n not in memo:
+            memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
+        return memo[n]
