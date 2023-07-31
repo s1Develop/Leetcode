d0071e1a76e38cdb595c4d9d5bceb83fbda0e490
s1Develop <sungwon.dev@gmail.com>
2023-07-30 23:35:50 -0700

    07/30/2023

---
+++ Easy/ClimbStairs.py
@@ -0,0 +1,31 @@
+"""You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+"""
+
+#결국 어렵게 생각할 필요가 없다.
+#그냥 recursion을 써서 돌리면 끝...
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 1
+        else:
+            return self.climbStairs(n-1) + self.climbStairs(n-2)
