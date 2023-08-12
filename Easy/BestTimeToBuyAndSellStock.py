11422a460d685f7acf0d41de77223dab1b34d65b
s1Develop <sungwon.dev@gmail.com>
2023-08-11 21:41:12 -0700

    08/11/2023

--- Easy/BestTimeToBuyAndSellStock.py
+++ Easy/BestTimeToBuyAndSellStock.py
@@ -47,31 +47,4 @@ class Solution:
                     # val값을 price[i] - temp값으로 업데이트해준다
                     val = prices[i]-temp
 
-        return valclass Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-
-        # 일단 temp를 prices의 첫 element로 잡고
-        temp = prices[0]
-
-        # value값을 0으로 잡는다
-        val = 0
-
-        # loop를 0부터 prices의 마지막 index까지 돌린다
-        for i in range(len(prices)):
-
-            # 현재 temp값이 현재 prices의 i index의 값보다 크다고 하면
-            if temp > prices[i]:
-
-                # temp값을 현재 prices의 i index의 값으로 바꾼다
-                temp = prices[i]
-
-            # 만약 temp값이 prices의 i index의 값보다 작거나 같으면
-            elif temp <= prices[i]:
-
-                # 거기서 prices의 i index의 값이랑 temp값을 비교했을때 현재 val값보다 클 경우
-                if (prices[i] - temp) > val:
-
-                    # val값을 price[i] - temp값으로 업데이트해준다
-                    val = prices[i]-temp
-
         return val
