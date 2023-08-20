de7ada55355634737c382a548037483c91887377
s1Develop <sungwon.dev@gmail.com>
2023-08-20 00:01:50 -0700

    08/19/2023 add more explanation

--- Easy/ExcelSheetColumnNumber.py
+++ Easy/ExcelSheetColumnNumber.py
@@ -26,6 +26,9 @@ Example 3:
 Input: columnTitle = "ZY"
 Output: 701
 """
+"""
+given a string of length 1, the ord() function returns an integer representing the Unicode code point of the character when an argument is a Unicode object, or the value of the byte when the argument is an 8-bit string
+"""
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
 
