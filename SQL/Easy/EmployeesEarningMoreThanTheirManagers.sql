576bf3ec58a7a3ca03aadf9f98cdd643dc0ea91d
s1Develop <sungwon.dev@gmail.com>
2023-08-21 09:56:43 -0700

    08/21/2023 (2)

---
+++ SQL/Easy/EmployeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,48 @@
+-- Table: Employee
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | salary      | int     |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+
+-- Write a solution to find the employees who earn more than their managers.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Employee table:
+-- +----+-------+--------+-----------+
+-- | id | name  | salary | managerId |
+-- +----+-------+--------+-----------+
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | Null      |
+-- | 4  | Max   | 90000  | Null      |
+-- +----+-------+--------+-----------+
+-- Output: 
+-- +----------+
+-- | Employee |
+-- +----------+
+-- | Joe      |
+-- +----------+
+-- Explanation: Joe is the only employee who earns more than his manager.
+
+-- # Write your MySQL query statement below
+-- SELECT는 return할것, FROM은 테이블과 그 테이블을 칭할 것
+-- WHERE은 조건들
+SELECT e1.name AS 'Employee'
+FROM Employee e1, Employee e2
+WHERE e1.managerId = e2.id and e1.salary > e2.salary
