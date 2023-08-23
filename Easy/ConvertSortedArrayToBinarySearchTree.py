5dfabeb9054f25c6337fd2f7f1d100ab0723d271
s1Develop <sungwon.dev@gmail.com>
2023-08-23 11:37:58 -0700

    08/22/2023

--- Easy/ConvertSortedArrayToBinarySearchTree.py
+++ Easy/ConvertSortedArrayToBinarySearchTree.py
@@ -20,7 +20,6 @@ Output: [3,1]
 Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
 """
 
-
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
