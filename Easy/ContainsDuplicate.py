70a5d569d28b053d1c4b1fb7742cce463d63db52
s1Develop <sungwon.dev@gmail.com>
2023-08-30 22:43:02 -0700

    08/30/2023

---
+++ Easy/ContainsDuplicate.py
@@ -0,0 +1,31 @@
+"""
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+"""
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        save = {}
+
+        for i in nums:
+            if i in save:
+                return True
+            # save[i] refers to value associated with the key i 그니까 i가 key이고 save[i]라고 하면 value
+            # 그래서 save[i] = j 이라고 하면 i라는 key에 j를 value로 넣는다는 말
+            save[i] = i
+        return False
