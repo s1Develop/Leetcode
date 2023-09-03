55da4ba1d196dd1d926ef012249238118849a1c9
s1Develop <sungwon.dev@gmail.com>
2023-09-02 23:39:16 -0700

    09/02/2023

---
+++ Easy/BinaryTreePaths.py
@@ -0,0 +1,42 @@
+"""
+Given the root of a binary tree, return all root-to-leaf paths in any order.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,null,5]
+Output: ["1->2->5","1->3"]
+Example 2:
+
+Input: root = [1]
+Output: ["1"]
+"""
+
+from typing import Optional, List
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        # BFS
+
+        def helper(root ,path):
+            if root:
+                path += str(root.val)
+                if not root.left and not root.right:
+                    paths.append(path)
+                else:
+                    path += '->'
+                    helper(root.left, path)
+                    helper(root.right, path)
+        paths = []
+        helper(root, '')
+        return paths
+        
