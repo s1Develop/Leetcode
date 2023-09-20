cbc4c890130c0f479c25f5f6a60855cc9dd12e33
s1Develop <sungwon.dev@gmail.com>
2023-09-20 11:36:31 -0700

    09/20/2023

---
+++ Easy/ReverseString.py
@@ -0,0 +1,35 @@
+"""
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+"""
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def reverse_helper(left, right) -> None:
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                reverse_helper(left+1, right-1)
+        return reverse_helper(0, len(s)-1)
+        # s.reverse() will work but just for practice.
