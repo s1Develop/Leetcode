ebf0c6ee56dcdb1277b52cdd345f1803f0858d0f
s1Develop <sungwon.dev@gmail.com>
2023-09-23 23:58:44 -0700

    09/23/2023

--- Easy/ReverseVowelsOfAString.py
+++ Easy/ReverseVowelsOfAString.py
@@ -29,14 +29,18 @@ class Solution:
 
         vowels_stack = []
 
-        sorted = ''
+        temp = ''
 
+        # 먼저 s안에 있는 모든 vowels들을 vowels_stack안에 다 넣는다
         for char in s:
             if char in vowels:
                 vowels_stack.append(char)
+
+        # 다시 loop를 돌려서 s안에서 vowel이라면 stack안에서 pop되는 애(즉 s안에서 마지막에 있던 애를)를 temp안에 넣어준다
+        # 만약 vowel이 아니라면 그냥 그대로 각각 temp안에 각각의 current index in s를 넣어준다.
         for char in s:
             if char in vowels:
-                sorted += vowels_stack.pop()
+                temp += vowels_stack.pop()
             else:
-                sorted += char
-        return sorted
+                temp += char
+        return temp
