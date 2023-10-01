9cc41fa10cc1eb2b4c33ed24512f60eb73d848a1
s1Develop <sungwon.dev@gmail.com>
2023-09-30 19:57:47 -0700

    09/30/2023

---
+++ Medium/MaximumProductSubarray.py
@@ -0,0 +1,41 @@
+"""
+Given an integer array nums, find a 
+subarray
+ that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+"""
+from typing import List
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+       if len(nums) == 0:
+           return 0
+
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+
+        for i in range(i, len(nums)):
+            curr = nums[i]
+            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
+            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
+
+            max_so_far = temp_max
+
+            result = max(max_so_far, result)
+
+        return result
